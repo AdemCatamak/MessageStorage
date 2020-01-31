807710195c6b99faa5e613a01ce633aa8e0ff2ca
ademcatamak <ademcatamak@gmail.com>
2020-01-31 17:35:03 +0300

    message-storage-db-mssql tests

---
+++ Tests/MessageStorage.Db.MsSqlUnitTests/MsSqlDbStorageAdaptor_CreateConnectionTests.cs
@@ -0,0 +1,38 @@
+using System.Data;
+using MessageStorage.Db.MsSql;
+using MessageStorage.Exceptions;
+using Moq;
+using Xunit;
+
+namespace MessageStorage.Db.MsSqlUnitTests
+{
+    public class MsSqlDbStorageAdaptor_CreateConnectionTests
+    {
+        private readonly MsSqlDbStorageAdaptor _sut;
+
+        public MsSqlDbStorageAdaptor_CreateConnectionTests()
+        {
+            var mockMsSqlMigrationRunner = new Mock<IMsSqlMigrationRunner>();
+            _sut = new MsSqlDbStorageAdaptor(mockMsSqlMigrationRunner.Object);
+        }
+
+        [Fact]
+        public void WhenCreateConnectionExecutedWithoutExecutedSetConfiguration__PreconditionFailedExceptionOccurs()
+        {
+            Assert.Throws<PreConditionFailedException>(() => _sut.CreateConnection());
+        }
+
+        [Fact]
+        public void WhenDbConfigurationSetOperationExecuted__CreateConnectionExecuted__DbConnectionReturnClosedState()
+        {
+            const string connectionStr = "Server=localhost,1433;Database=TestDb;User=sa;Password=Passw0rd;Trusted_Connection=False;";
+            MessageStorageDbConfiguration messageStorageDbConfiguration = MessageStorageDbConfigurationFactory.Create(connectionStr);
+            _sut.SetConfiguration(messageStorageDbConfiguration);
+
+            IDbConnection dbConnection = _sut.CreateConnection();
+            Assert.NotNull(dbConnection);
+            Assert.Equal(connectionStr, dbConnection.ConnectionString);
+            Assert.Equal(ConnectionState.Closed, dbConnection.State);
+        }
+    }
+}
---
+++ Tests/MessageStorage.Db.MsSqlUnitTests/MsSqlDbStorageAdaptor_SetConfigurationTests.cs
@@ -0,0 +1,33 @@
+using System;
+using MessageStorage.Db.MsSql;
+using MessageStorage.Exceptions;
+using Moq;
+using Xunit;
+
+namespace MessageStorage.Db.MsSqlUnitTests
+{
+    public class MsSqlDbStorageAdaptor_SetConfigurationTests
+    {
+        private readonly MsSqlDbStorageAdaptor _sut;
+
+        public MsSqlDbStorageAdaptor_SetConfigurationTests()
+        {
+            var mockMsSqlMigrationRunner = new Mock<IMsSqlMigrationRunner>();
+            _sut = new MsSqlDbStorageAdaptor(mockMsSqlMigrationRunner.Object);
+        }
+
+        [Fact]
+        public void WhenSetConfigurationExecutedWithNull__ArgumentNullExceptionOccurs()
+        {
+            Assert.Throws<ArgumentNullException>(() => _sut.SetConfiguration(null));
+        }
+
+        [Fact(Skip = "This validation is not executed yet. Maybe MessageStorageDbConfiguration is converted to abstract and concretes created via IMsSqlDbConnection")]
+        public void WhenDbConfigurationSetOperationExecuted_InvalidConnectionStr__DbConnectionReturnClosedState()
+        {
+            const string connectionStr = "dummy-connection-str";
+            MessageStorageDbConfiguration messageStorageDbConfiguration = MessageStorageDbConfigurationFactory.Create(connectionStr);
+            Assert.Throws<ConnectionStringValidationException>(() => _sut.SetConfiguration(messageStorageDbConfiguration));
+        }
+    }
+}
