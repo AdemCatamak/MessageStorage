3a5b8770e420b437aa79ca02382ec2ec1044ef25
ademcatamak <ademcatamak@gmail.com>
2020-07-16 23:52:00 +0300

    remove unused hosted-service

--- MessageStorage.AspNetCore/JobProcessorHostedService.cs
+++ MessageStorage.AspNetCore/JobProcessorHostedService.cs
@@ -1,18 +1,10 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MessageStorage.Clients;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace MessageStorage.AspNetCore
 {
-    public class JobProcessorHostedService : JobProcessorHostedService<IJobProcessor>
-    {
-        public JobProcessorHostedService(IJobProcessor jobProcessor) : base(jobProcessor)
-        {
-        }
-    }
-
     public class JobProcessorHostedService<TJobProcessor> : IHostedService
         where TJobProcessor : IJobProcessor
     {
@@ -31,21 +23,6 @@ namespace MessageStorage.AspNetCore
         public async Task StopAsync(CancellationToken cancellationToken)
         {
             await _jobProcessor.StopAsync(cancellationToken);
-        }
-    }
-
-    public static class JobProcessorHostedServiceExtension
-    {
-        public static IServiceCollection AddJobProcessorHostedService(this IServiceCollection serviceCollection)
-        {
-            return AddJobProcessorHostedService<IJobProcessor>(serviceCollection);
-        }
-
-        public static IServiceCollection AddJobProcessorHostedService<TJobProcessor>(this IServiceCollection serviceCollection)
-            where TJobProcessor : IJobProcessor
-        {
-            serviceCollection.AddHostedService<JobProcessorHostedService<TJobProcessor>>();
-            return serviceCollection;
         }
     }
 }
