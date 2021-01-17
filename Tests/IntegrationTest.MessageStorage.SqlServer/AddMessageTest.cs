20de07101d5a288a9398f26b4a2c64ed33c16f65
ademcatamak <ademcatamak@gmail.com>
2021-01-17 09:57:30 +0300

    transaction test

--- Tests/IntegrationTest.MessageStorage.SqlServer/AddMessageTest.cs
+++ Tests/IntegrationTest.MessageStorage.SqlServer/AddMessageTest.cs
@@ -42,13 +42,142 @@ namespace IntegrationTest.MessageStorage.SqlServer
                                                           });
                 Assert.Equal(1, count);
             }
+        }
+
+        [Fact]
+        public void When_AddMessage_TransactionNotCommitted__MessageShouldNotBePersisted()
+        {
+            object myObj = "dummy-message";
+            using (IDbConnection connection = _sqlServerTestFixture.CreateDbConnection())
+            {
+                connection.Open();
+                string messageId;
+                using (IDbTransaction dbTransaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    _sut.UseTransaction(dbTransaction);
+                    var (message, _) = _sut.Add(myObj);
+                    messageId = message.Id;
+
+                    var countInTransaction = connection.ExecuteScalar<int>($"Select count(*) from [{SqlServerTestFixture.SCHEMA}].[Messages] where MessageId = @messageId",
+                                                                           new
+                                                                           {
+                                                                               messageId
+                                                                           },
+                                                                           dbTransaction);
+                    Assert.Equal(1, countInTransaction);
+                }
+
+                var count = connection.ExecuteScalar<int>($"Select count(*) from [{SqlServerTestFixture.SCHEMA}].[Messages] where MessageId = @messageId",
+                                                          new
+                                                          {
+                                                              messageId
+                                                          });
+                Assert.Equal(0, count);
+            }
+        }
+
+        [Fact]
+        public void When_AddMessage_MessageStorageTransactionCommitted__MessageShouldBePersisted()
+        {
+            object myObj = "dummy-message";
+            using (IDbConnection connection = _sqlServerTestFixture.CreateDbConnection())
+            {
+                connection.Open();
+                string messageId;
+                using (var dbTransaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    IMessageStorageTransaction messageStorageTransaction = _sut.UseTransaction(dbTransaction);
+                    var (message, _) = _sut.Add(myObj);
+                    messageId = message.Id;
+                    messageStorageTransaction.Commit();
+
+                    var countInTransaction = connection.ExecuteScalar<int>($"Select count(*) from [{SqlServerTestFixture.SCHEMA}].[Messages] where MessageId = @messageId",
+                                                                           new
+                                                                           {
+                                                                               messageId
+                                                                           },
+                                                                           dbTransaction);
+                    Assert.Equal(1, countInTransaction);
+                }
+
+                var count = connection.ExecuteScalar<int>($"Select count(*) from [{SqlServerTestFixture.SCHEMA}].[Messages] where MessageId = @messageId",
+                                                          new
+                                                          {
+                                                              messageId
+                                                          });
+                Assert.Equal(1, count);
+            }
+        }
+
+        [Fact]
+        public void When_AddMessage_MessageStorageTransactionCommitted__MessageStorageClientShouldBeAcceptNewTransaction()
+        {
+            object myObj = "dummy-message";
+            using (IDbConnection connection = _sqlServerTestFixture.CreateDbConnection())
+            {
+                connection.Open();
+                string messageId1, messageId2;
+                using (IDbTransaction dbTransaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    IMessageStorageTransaction messageStorageTransaction = _sut.UseTransaction(dbTransaction);
+                    var (message, _) = _sut.Add(myObj);
+                    messageId1 = message.Id;
+                    messageStorageTransaction.Commit();
+                }
+
+                using (IDbTransaction dbTransaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    IMessageStorageTransaction messageStorageTransaction = _sut.UseTransaction(dbTransaction);
+                    var (message, _) = _sut.Add(myObj);
+                    messageId2 = message.Id;
+                    messageStorageTransaction.Commit();
+                }
+
+                var count = connection.ExecuteScalar<int>($"Select count(*) from [{SqlServerTestFixture.SCHEMA}].[Messages] where MessageId in (@messageId1, @messageId2)",
+                                                          new
+                                                          {
+                                                              messageId1, messageId2
+                                                          }
+                                                         );
+                Assert.Equal(2, count);
+            }
+        }
+
+
+        [Fact]
+        public void When_AddMessage_MessageStorageTransactionCommitted__MessageStorageClientShouldBeDisposed()
+        {
+            dotMemory.Check(memory => Assert.Equal(1,
+                                                   memory.GetObjects(o => o.Type.Is<IMessageStorageClient>())
+                                                         .ObjectsCount
+                                                  )
+                           );
+
+            object myObj = "dummy-message";
+            using (IDbConnection connection = _sqlServerTestFixture.CreateDbConnection())
+            {
+                connection.Open();
+                using (IDbTransaction dbTransaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    IMessageStorageTransaction messageStorageTransaction = _sut.UseTransaction(dbTransaction);
+                    _sut.Add(myObj);
+                    messageStorageTransaction.Commit();
+                }
+            }
 
             dotMemory.Check(memory => Assert.Equal(0,
-                                                   memory.GetObjects(o => o.Type.Is<IDbConnection>()).ObjectsCount
+                                                   memory.GetObjects(o => o.Type.Is<IDbConnection>())
+                                                         .ObjectsCount
                                                   )
                            );
             dotMemory.Check(memory => Assert.Equal(0,
-                                                   memory.GetObjects(o => o.Type.Is<IDbTransaction>()).ObjectsCount
+                                                   memory.GetObjects(o => o.Type.Is<IDbTransaction>())
+                                                         .ObjectsCount
+                                                  )
+                           );
+            dotMemory.Check(memory => Assert.Equal(0,
+                                                   memory.GetObjects(o => o.Type.Is<IMessageStorageClient>())
+                                                         .ObjectsCount
                                                   )
                            );
         }
